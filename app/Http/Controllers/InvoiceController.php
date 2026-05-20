e9aa29d3dc8174401b0460ef3e4fb238555c4c23
chandan18171 <152905242+chandan18171@users.noreply.github.com>
2026-05-20 15:15:47 +0530

    Upload InvoiceController.php

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $invoices = \Illuminate\Support\Facades\Auth::user()->role === 'admin' 
+            ? \App\Models\Invoice::with(['user', 'reading.meter'])->get() 
+            : \Illuminate\Support\Facades\Auth::user()->invoices()->with('reading.meter')->get();
+        return view('invoices.index', compact('invoices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invoice $invoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoice $invoice)
+    {
+        //
+    }
+}
